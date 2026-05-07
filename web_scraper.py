555927b74757e3c05eec8914e5c9e8b4b7ca0f5b
Usha348 <usharm3587@gmail.com>
2026-05-07 15:01:13 +0530

    Add files via upload

---
+++ web_scraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+quotes = soup.find_all("span", class_="text")
+authors = soup.find_all("small", class_="author")
+
+for i in range(len(quotes)):
+    print("Quote:", quotes[i].text)
+    print("Author:", authors[i].text)
+    print()
